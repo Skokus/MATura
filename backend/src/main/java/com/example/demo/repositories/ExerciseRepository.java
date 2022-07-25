699dbae2470aeb5ff08eef3fd2b53cbca4813b61
Paweł Cegielski <skok2022@gmail.com>
2022-07-25 22:33:27 +0200

    Starting backend

--- backend/src/main/java/com/example/demo/repositories/ExerciseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.Exercise;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExerciseRepository extends MongoRepository<Exercise, String> {
-}
