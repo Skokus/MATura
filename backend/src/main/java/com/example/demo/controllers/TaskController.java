ee1d3ede64014592dae4da042df9478ce2270489
Paweł Cegielski <skok2022@gmail.com>
2022-07-31 22:44:19 +0200

    Adding GET Task

--- backend/src/main/java/com/example/demo/controllers/TaskController.java
+++ backend/src/main/java/com/example/demo/controllers/TaskController.java
@@ -13,17 +13,26 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "tasks")
+@RequestMapping(value = "/tasks")
 public class TaskController {
     @Autowired
     private CategoryRepository categoryRepository;
     @Autowired
     private TaskRepository taskRepository;
-    @RequestMapping(value = "/tasks/{categoryName}", method = RequestMethod.POST)
+    @RequestMapping(value = "/{categoryName}", method = RequestMethod.POST)
     public ResponseEntity<Category> saveTask(@RequestBody Task task, @PathVariable String categoryName){
         Category c = categoryRepository.findByName(categoryName);
         Task t = taskRepository.save(task);
         c.getTasks().add(t);
         return new ResponseEntity<>(categoryRepository.save(c), HttpStatus.CREATED);
+    }
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Optional<Task> getTaskById(@PathVariable String id){
+        return taskRepository.findById(id);
+    }
+    @RequestMapping(value = "/{categoryName}/{id}", method = RequestMethod.GET)
+    public Task getTaskByNumberInCategory(@PathVariable String categoryName, @PathVariable int id){
+        Category c = categoryRepository.findByName(categoryName);
+        return c.getTasks().get(id);
     }
 }
