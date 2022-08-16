b96223dfb889ff4c72ec8bab202f5550531d19b4
Paweł Cegielski <skok2022@gmail.com>
2022-08-16 20:16:07 +0200

    Category links basic styling and path

--- frontend/mat_ura/src/categories/Category.js
+++ frontend/mat_ura/src/categories/Category.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import "./Category.css"
 import {Link} from "react-router-dom";
+import icon from "./iconplaceholder.png";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import "../bootstrap/bootstrap.css";
 
 //, { useState, useEffect }
 function Category(props){    
     return(
-        <div>
-            <h2>{props.name}</h2>
-            <Link to="/tasks/1"> Go to about</Link>
+        <div className = "category">
+            <img src={icon} alt={props.name}/>
+            <div className="inline">
+                <h2>{props.name}</h2><br></br>
+                <ProgressBar now={60}/>
+            </div>
+            <Link to={"/categories/" + props.name}> Go to about</Link>
         </div>
     );
 }
