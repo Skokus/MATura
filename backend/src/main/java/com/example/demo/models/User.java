14562548a6d8f7c365782257cb6951f5ab0d4811
Paweł Cegielski <skok2022@gmail.com>
2022-08-29 20:37:36 +0200

    Adding User entity

---
+++ backend/src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,81 @@
+package com.example.demo.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    private String email;
+    private String username;
+
+    private String password;
+
+    private String role;
+
+    private UserProgress userProgress;
+
+    public User(){
+
+    }
+
+    public User(String email, String username, String password, String role){
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.userProgress = new UserProgress();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public UserProgress getUserProgress() {
+        return userProgress;
+    }
+
+    public void setUserProgress(UserProgress userProgress) {
+        this.userProgress = userProgress;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
